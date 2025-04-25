7e4dd0e818613a6d60d9ab323c9c159579844bd9
mngono <bepeneru13@gmail.com>
2025-04-25 18:58:39 +0200

    test pour addd une image dockerfile 3

--- src/main/java/com/neru/contactfolio/controller/ContactController.java
+++ src/main/java/com/neru/contactfolio/controller/ContactController.java
@@ -3,13 +3,17 @@ package com.neru.contactfolio.controller;
 import com.neru.contactfolio.entities.ContactForm;
 import com.neru.contactfolio.service.EmailService;
 import jakarta.mail.MessagingException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("api/contact")
-@CrossOrigin(origins = "https://marvelous-biscochitos-8b098a.netlify.app")
+@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "https://marvelous-biscochitos-8b098a.netlify.app")
+
 public class ContactController {
 
     private EmailService emailService;
@@ -19,14 +23,19 @@ public class ContactController {
         this.emailService = emailService;
     }
 
-    @PostMapping
-    public ResponseEntity<String> envoyerMessage(@RequestBody ContactForm contactForm) throws MessagingException {
+    @PostMapping( consumes =MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+
+    public ResponseEntity<String> envoyerMessage(@ModelAttribute ContactForm contactForm) {
         try {
             emailService.envoyer(contactForm);
             return ResponseEntity.ok("Message envoyé avec succès !");
+        } catch (MailException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Erreur d'envoi de l'email : " + e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace(); // Pour avoir plus de détails en console
-            return ResponseEntity.status(500).body("Erreur lors de l'envoi de l'email : " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Une erreur est survenue : " + e.getMessage());
         }
     }
+
 }
